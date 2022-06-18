6dfa44beac3ed21cd960e603d8250e3231aaab5f
Herrzekt <jonathangnzlz2006@gmail.com>
2022-06-17 23:18:21 -0400

    Mini proyecto del curso

---
+++ mini_proyecto/src/mini_proyecto/Mini_proyecto.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mini_proyecto;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Mini_proyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ventana_principal A = new ventana_principal();
+        A.setVisible(true);
+    }
+}
---
+++ mini_proyecto/src/mini_proyecto/Switch.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mini_proyecto;
+
+/**
+ *
+ * @author Jonathan
+ */
+public class Switch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+}
